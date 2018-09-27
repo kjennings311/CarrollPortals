cf200f88b335837f27ab88fcf2a83d431257b2b2
Pavan Nanduri <Pavan.Nanduri@carrollorg.com>
2018-09-27 10:10:49 -0400

    no changes

--- Carroll.Portals/Helpers/AdminFilter.cs
+++ Carroll.Portals/Helpers/AdminFilter.cs
@@ -14,10 +14,14 @@ namespace Carroll.Portals.Helpers
             base.OnActionExecuting(filterContext);
             HttpContextBase ctx = filterContext.HttpContext;
            
-            if (!(filterContext.HttpContext.Session["Carroll_RoleName"].ToString().Contains("Administrator")))
+            if(Carroll.Portals.Models.LoggedInUser.AssignedRole().ToLower() != "administrator")
             {
-                filterContext.Result = new ContentResult() { Content = "UnAuthorised Access to Specified Resource" };
+                filterContext.Result = new ContentResult() { Content = "You do not have permission to access this resource. Please contact your IT department." };
             }
+            //if (!(filterContext.HttpContext.Session["Carroll_RoleName"].ToString().Contains("Administrator")))
+            //{
+            //    filterContext.Result = new ContentResult() { Content = "UnAuthorised Access to Specified Resource" };
+            //}
         }
 
     }
