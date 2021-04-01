17853c016c8a3e08e5879bc0e2db1d019908c50e
Sekhar Babu <sekhar.babu@carrollorg.com>
2021-04-01 21:42:40 +0530

    update

--- Carroll.Portals/Scripts/app/custom.js
+++ Carroll.Portals/Scripts/app/custom.js
@@ -1,5 +1,5 @@
 ﻿  var $BaseApiUrl = "http://localhost:1002/";
-// var $BaseApiUrl = "http://aspnet.carrollaccess.net:1002/";
+ //var $BaseApiUrl = "http://aspnet.carrollaccess.net:1002/";
 
 
 //49786/";
