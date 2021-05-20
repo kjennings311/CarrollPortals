9dc48d28d6931a309d6095d0907042fb7e494264
Sekhar Babu <sekhar.babu@carrollorg.com>
2021-05-20 22:50:12 +0530

    print 2

--- Carroll.Portals/Controllers/HrController.cs
+++ Carroll.Portals/Controllers/HrController.cs
@@ -41,7 +41,7 @@ namespace Carroll.Portals.Controllers
         {
             Baseurl = ConfigurationManager.AppSettings["ServiceURL"];
             //remove before live
-          //  Baseurl = "http://localhost:1002/";
+           Baseurl = "http://localhost:1002/";
         }
 
         public ActionResult Index()
@@ -1272,7 +1272,7 @@ namespace Carroll.Portals.Controllers
 
                 ViewBag.isprint = false;
                 //returning the employee list to view  
-                return new ViewAsPdf("PrintPayRollStatusChange", obj) { PageSize = Size.A4, CustomSwitches = "--disable-smart-shrinking", FileName = "PayrollStatusChange-" + obj.SequenceNumber + "-" + obj.EmployeeName + ".pdf" };
+                return new ViewAsPdf("PrintPayRollStatusChange2", obj) { PageSize = Size.A4, CustomSwitches = "--disable-smart-shrinking", FileName = "PayrollStatusChange-" + obj.SequenceNumber + "-" + obj.EmployeeName + ".pdf" };
 
             }
 
